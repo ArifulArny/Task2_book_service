4457552bcabb10815f17c6033efe55bb03c33f01
Ariful.Arny10 <100965129+ArifulArny@users.noreply.github.com>
2023-11-20 23:41:31 +0600

    Update models.py

--- models.py
+++ models.py
@@ -1,1 +1,14 @@
- 
+ from pydantic import BaseModel
+
+
+class Author(BaseModel):
+    full_name: str
+
+
+class Book(BaseModel):
+    title: str
+    author_id: int
+
+
+class Client(BaseModel):
+    full_name: str
